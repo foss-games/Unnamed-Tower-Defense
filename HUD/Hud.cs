c8807752f4d256c48291d180e5a7b2cd96a5ae1b
Arron Chapman <unkwntech@gmail.com>
2025-06-02 13:00:11 -0500

    More cleanup

--- HUD/Hud.cs
+++ HUD/Hud.cs
@@ -9,10 +9,16 @@ public partial class Hud : Node2D
     private int _currentWave;
     private int _maxWaves;
 
+    private int _minutes = 0;
+    private int _seconds = 0;
+
+    private Timer timer;
+
     public override void _Ready()
     {
         TimerText = GetNode<Label>("TimerText");
         WaveText = GetNode<Label>("WaveText");
+        timer = GetNode<CountdownTimer>("HudTimer");
     }
 
     public void SetWaves(int current, int max)
@@ -25,7 +31,32 @@ public partial class Hud : Node2D
 
     public void SetTimer(int seconds)
     {
-        
+        _seconds = seconds % 60;
+        _minutes = seconds / 60; //_minutes is int, so the decimal portion is lost automatically
+        UpdateTimerText();
     }
 
+    public void UpdateTimerText()
+    {
+        TimerText.Text = _minutes.ToString("D2") + ":" + _seconds.ToString("D2");
+    }
+
+    public void OnTimerTimeout()
+    {
+        if (_seconds == 0 && _minutes == 0)
+        {
+            timer.Stop();
+            return;
+        }
+
+        if (_seconds == 0)
+        {
+            _seconds = 59;
+            _minutes--;
+            return;
+        }
+
+        _seconds--;
+        UpdateTimerText();
+    }
 }
