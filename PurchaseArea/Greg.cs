d4e4f1c3346763cf74a8e1be3afa4dba94da96b0
Arron Chapman <unkwntech@gmail.com>
2025-06-10 21:38:28 -0500

    Towers can no longer be placed in illegal places.

--- PurchaseArea/Greg.cs
+++ PurchaseArea/Greg.cs
@@ -14,6 +14,8 @@ public partial class Greg : Node2D
     private TileMapLayer map;
     private Node2D towerCollection;
     private Tower tower;
+    private Play play;
+
     public override void _Ready()
     {
         StartingPosition = GlobalPosition;
@@ -24,10 +26,18 @@ public partial class Greg : Node2D
         button = GetNode<Button>("Button");
         button.ButtonDown += TowerPickedUp;
         button.ButtonUp += TowerDropped;
+        play = (Play)GetTree().GetFirstNodeInGroup("play");
     }
 
     public void TowerPickedUp()
     {
+        GD.Print(play.Credits);
+        GD.Print(tower.Cost);
+        if (play.Credits < tower.Cost)
+        {
+            GD.Print("No");
+            return;
+        }
         if (!IsHeld)
         {
             GD.Print("Grabbed");
@@ -38,6 +48,7 @@ public partial class Greg : Node2D
 
     public void TowerDropped()
     {
+        if (!IsHeld) return;
         GD.Print("Dropped");
         //GD.Print(map.MapToLocal(map.LocalToMap(GetGlobalMousePosition())));
         IsHeld = false;
@@ -47,7 +58,7 @@ public partial class Greg : Node2D
         tower.GetNode<Sprite2D>("Circle").Visible = false;
 
         GlobalPosition = StartingPosition;
-
+        //GD.Print(newTower.Cost);
         ((Play)GetTree().GetFirstNodeInGroup("play")).Credits -= newTower.Cost;
     }
 
