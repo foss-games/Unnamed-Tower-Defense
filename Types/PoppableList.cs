f8c9c0fc6a95c052cb0c5655b40f0ae572f1028f
Arron Chapman <unkwntech@gmail.com>
2025-06-19 21:30:52 -0500

    Updated UI

--- Types/PoppableList.cs
+++ Types/PoppableList.cs
@@ -1,22 +1,30 @@
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
+// using System.ComponentModel;
+// using System.Text.Json;
+// using System.Text.Json.Serialization;
 
-namespace FOSSGames
-{
-    public class PoppableList<T> : List<T>
-    {
-        public T PopFirst()
-        {
-            T item = this[0];
-            Remove(item);
-            return item;
-        }
-        public T Pop()
-        {
-            T item = this[Count - 1];
-            Remove(item);
-            return item;
-        }
+// namespace FOSSGames
+// {
+//     public class PoppableList<T> : List<T>
+//     {
+//         public PoppableList() : base()
+//         {
 
-        public T PopLast() => Pop();
-    }
-}
+//         }
+//         public T PopFirst()
+//         {
+//             T item = this[0];
+//             Remove(item);
+//             return item;
+//         }
+//         public T Pop()
+//         {
+//             T item = this[Count - 1];
+//             Remove(item);
+//             return item;
+//         }
+
+//         public T PopLast() => Pop();
+//     }
+// }
