f51c5cd87e7f0ae8ce8ef9978eb7f3db6e312a2d
Arron Chapman <unkwntech@gmail.com>
2025-06-30 19:45:06 -0500

    Cleanup

--- Main Menu/MainMenu.cs
+++ Main Menu/MainMenu.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text.Json;
 using FOSSGames;
 using Godot;
 
@@ -11,10 +13,12 @@ public partial class MainMenu : Node2D
         //create buttons and add to GridContainer
         for (int i = 0; i < Global.Instance.Levels.Count; i++)
         {
-            CustomDataButton button = new CustomDataButton();
-            button.CustomData = i;
+            CustomDataButton button = new()
+            {
+                CustomData = i,
+                Text = $"Level {i + 1}"
+            };
             button.PressedWithData += OnLevelSelection;
-            button.Text = $"Level {i + 1}";
             button.AddThemeFontSizeOverride("font_size", 30);
             gridContainer.AddChild(button);
         }
@@ -23,6 +27,19 @@ public partial class MainMenu : Node2D
         {
             GetNode<Button>("MapMakerButton").Visible = true;
         }
+
+        //string json = JsonSerializer.Serialize<Level>(Global.Instance.Levels[0], SourceGenerationContext.Default.Level);
+
+        // Wave w = new Wave();
+        // w.Interval = 3;
+        // w.Enemies = [];
+        // w.Enemies.Add(new WaveEnemy() { GUID = Guid.NewGuid() });
+        // w.Enemies.Add(new WaveEnemy() { GUID = Guid.NewGuid() });
+
+        //GD.Print(Json.Stringify(w));
+
+        // GD.Print(w.ToJSON());
+        return;
     }
 
     public void OnPlayButton()
